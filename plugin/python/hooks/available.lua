16a0daabd2e9b0518f7c6a6210432598ca3ec0df
janbar <janbar@qq.com>
2026-01-09 16:32:12 +0800

    删除python，后续使用uv工具

--- plugin/python/hooks/available.lua
+++
@@ -1,8 +0,0 @@
-require("util")
-function PLUGIN:Available(ctx)
-    if OS_TYPE == "windows" then
-        return parseVersion()
-    else
-        return parseVersionFromPyenv()
-    end
-end
--- plugin/python/hooks/post_install.lua
+++
@@ -1,8 +0,0 @@
-require("util")
-function PLUGIN:PostInstall(ctx)
-    if OS_TYPE == "windows" then
-        return windowsInstall(ctx)
-    else
-        return linuxCompile(ctx)
-    end
-end
